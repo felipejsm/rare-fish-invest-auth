884e7f172e41e1b687ed794cb42174b2e93bf553
felipejsm <felipejsm41@gmail.com>
2023-07-02 19:36:02 -0300

    Using firestore adapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { signOut, useSession } from 'next-auth/react';
 
+
 export default function Home() {
-  const session = useSession();
+  const {data : Session, status } = useSession();
+   if (status === "unauthenticated") {
+    return <p>Access Denied</p>
+  } 
   return (
     <>
-      <div >{session?.data?.user?.name }</div>
+      <div >{Session?.user?.name}</div>
       <button onClick={() => signOut()}>Logout</button>
     </>
   )
